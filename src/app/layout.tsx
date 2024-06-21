bf2ded26b9de88d67011a11e64d994af86aa1ebd
alexgoni <goni000211@gmail.com>
2024-06-21 09:47:41 +0900

    📦 chore: lang 속성 ko로 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <head>
         <link
           rel="stylesheet"
