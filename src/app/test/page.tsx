f11f14a6245794e659327e3fce4e6837df5eb7c0
alexgoni <goni000211@gmail.com>
2024-06-22 12:50:50 +0900

    ✨ feat: Default Input

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import TextInput from '@/components/commons/input/TextInput';
+import Input from '@/components/commons/input';
 import { useForm } from 'react-hook-form';
 
 export default function ComponentTest() {
@@ -8,11 +8,36 @@ export default function ComponentTest() {
 
   return (
     <form>
-      <label htmlFor="name">test</label>
-      <TextInput
+      <label htmlFor="name">닉네임</label>
+      <Input
         id="name"
-        placeholder="test"
+        type="text"
+        placeholder="닉네임"
         register={{ ...register('name', { required: true }) }}
+      />
+
+      <label htmlFor="email">이메일</label>
+      <Input
+        id="email"
+        type="email"
+        placeholder="이메일"
+        register={{ ...register('email', { required: true }) }}
+      />
+
+      <label htmlFor="email">비밀번호</label>
+      <Input
+        id="password"
+        type="password"
+        placeholder="비밀번호"
+        register={{ ...register('password', { required: true }) }}
+      />
+
+      <label htmlFor="email">비밀번호 확인</label>
+      <Input
+        id="password-confirm"
+        type="password"
+        placeholder="비밀번호 확인"
+        register={{ ...register('password-confirm', { required: true }) }}
       />
     </form>
   );
---
+++ src/components/commons/input/index.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import classNames from 'classnames';
+import Image from 'next/image';
+import { InputHTMLAttributes, useState } from 'react';
+import { UseFormRegisterReturn } from 'react-hook-form';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  type?: string;
+  error?: boolean;
+  register: UseFormRegisterReturn;
+}
+
+export default function Input({
+  type = 'text',
+  error,
+  register,
+  ...args
+}: InputProps) {
+  const classnames = classNames(
+    'block h-12 w-full rounded-md border px-4 outline-none placeholder:text-gray-300',
+    {
+      'border-red-dashboard focus:border-red-dashboard': error,
+      'border-gray-200 focus:border-toss-blue': !error,
+    },
+  );
+
+  if (type === 'password') {
+    return (
+      <PasswordInput className={classnames} register={register} {...args} />
+    );
+  }
+
+  return <input type={type} className={classnames} {...args} {...register} />;
+}
+
+interface PasswordInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  className: string;
+  register: UseFormRegisterReturn;
+}
+
+function PasswordInput({ className, register, ...args }: PasswordInputProps) {
+  const [pwClose, setPwClose] = useState(true);
+
+  const togglePWEye = () => {
+    setPwClose((prev) => !prev);
+  };
+
+  return (
+    <div className="relative">
+      <input
+        type={pwClose ? 'password' : 'text'}
+        className={className}
+        {...args}
+        {...register}
+      />
+      <Image
+        src={pwClose ? '/icon/visibility_off.svg' : '/icon/visibility_on.svg'}
+        alt="pw-eye"
+        width={24}
+        height={24}
+        onClick={togglePWEye}
+        className="absolute right-6 top-1/2 -translate-y-1/2 cursor-pointer"
+      />
+    </div>
+  );
+}
