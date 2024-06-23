3f04fb785ecc4dcb63d28fe2f42a4fed1eb67aac
JaesungKo <jaesungko.dev@gmail.com>
2024-06-24 05:47:16 +0900

    ✨ feat: Button Component에 onClick 프롭 추가 및 코드 개선

--- src/components/commons/button/index.tsx
+++ src/components/commons/button/index.tsx
@@ -6,19 +6,19 @@ type ButtonProps = {
   size: 'sm' | 'lg';
   text: string;
   cancled?: boolean;
+  onClick?: () => void;
 };
 
-export default function Button({ size, text, cancled }: ButtonProps) {
-  const sizeClasses = classNames({
+export default function Button({ size, text, cancled, onClick }: ButtonProps) {
+  const className = classNames('round rounded-8', {
     'text-16 px-46 py-14': size === 'lg',
-    'text-14 px-56 py-12 ': size === 'sm',
+    'text-14 px-56 py-12': size === 'sm',
+    'bg-white border border-gray-200 text-gray-400 hover:bg-gray-100': cancled,
+    'bg-toss-blue text-white hover:bg-blue-400': !cancled,
   });
-  const cancle = cancled
-    ? 'bg-white border border-gray-200 text-gray-400 hover:bg-gray-100'
-    : 'bg-toss-blue text-white hover:bg-blue-400';
 
   return (
-    <button className={`round rounded-8 ${sizeClasses} ${cancle}`}>
+    <button type="button" className={`${className}`} onClick={onClick}>
       {text}
     </button>
   );
