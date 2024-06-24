3f4ea83be67dd98b73e32fb9a2559a95ea02478a
Jaesung Ko <98534731+devjaesung@users.noreply.github.com>
2024-06-24 16:48:32 +0900

    [#21] Button Component (#22)
    * ✨ feat: Button 컴포넌트 구현
    
    * ✨ feat: Button Component에 onClick 프롭 추가 및 코드 개선
    
    * ✨ feat: AuthButton 컴포넌트 구현
    
    * ✨ feat: Bar Button 컴포넌트 구현
    
    * 💄 ui: 색상 변경 및 추가
    
    * ♻️ refactor: AuthButton 클래스네임 구조 변경, 코드 리팩토링
    
    * 🔥 remove: 테스트 파일 삭제

---
+++ src/components/commons/button/BarButton.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import classNames from 'classnames';
+import Image from 'next/image';
+
+type BarButtonProps = {
+  text?: string;
+  size?: 'sm' | 'lg';
+  weight?: 'string';
+  onClick?: () => void;
+};
+
+export default function BarButton({
+  text,
+  size = 'lg',
+  onClick,
+}: BarButtonProps) {
+  const className = classNames(
+    'w-full flex justify-center items-center text-black-800 border border-gray-200 gap-8 font-bold  hover:border-gray-400 ',
+    {
+      'py-24 rounded-8 text-18': size === 'lg' && text,
+      'py-9 rounded-6': size === 'lg' && !text,
+      'py-20 text-16 rounded-8': size === 'sm' && text,
+      'py-6 rounded-6': size === 'sm' && !text,
+    },
+  );
+  return (
+    <button type="button" className={className} onClick={onClick}>
+      {text}
+      <Image
+        width={22}
+        height={22}
+        src="/icon/add_box_color.svg"
+        alt="Bar Button"
+      />
+    </button>
+  );
+}
