e3a58603df0d11bca202dd4169f01d4823c3a925
yeonilil <gwagseoyeon@gmail.com>
2024-06-24 10:58:17 +0900

    style: 사이드메뉴 반응형

--- src/components/mydashboard/Sidemenu.tsx
+++ src/components/mydashboard/Sidemenu.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Sidemenu() {
   return (
     <>
-      <nav className="z-999 left-0 top-0 h-screen w-300 border-1 border-r-gray-200 bg-white pl-24 pt-20">
+      <nav className="z-999 left-0 top-0 h-screen w-67 border-1 border-r-gray-200 bg-white pl-20 pt-20 md:w-160 md:pl-24 lg:w-300 lg:pl-24">
         <Link
           href="/"
           className="cursor-pointer text-25 font-bold text-toss-blue md:hidden lg:hidden"
@@ -17,11 +17,11 @@ export default function Sidemenu() {
         >
           Planit:
         </Link>
-        <div className="mt-50 flex justify-between pr-24">
+        <div className="mt-50 flex items-center justify-between lg:pr-24">
           <p className="text-12 font-bold text-gray-400 sm:hidden">
             Dash Boards
           </p>
-          <button>
+          <button className="md:pr-12">
             <Image
               src="/icon/add_box.svg"
               width={20}
