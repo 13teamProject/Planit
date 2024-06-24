f345a25f8f31a1af0c46bf74689f4cda40b1d1d0
JaesungKo <jaesungko.dev@gmail.com>
2024-06-24 09:01:43 +0900

    ✨ feat: Modal 컴포넌트 구현

---
+++ src/components/commons/modal/index.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import classNames from 'classnames';
+
+type ModalProps = {
+  isOpen: boolean;
+  size?: 'sm' | 'md' | 'lg';
+  onClose: () => void;
+  children: React.ReactNode;
+};
+
+export default function Modal({
+  isOpen,
+  size = 'md',
+  onClose,
+  children,
+}: ModalProps) {
+  if (!isOpen) return null;
+
+  const handleBackgroundClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    // 배경 클릭 시 모달 닫기
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
+
+  const modalClass = classNames(
+    'rounded-8 relative w-full bg-white p-20 shadow-lg',
+    {
+      'max-w-lg': size === 'lg',
+      'max-w-md': size === 'md',
+      'max-w-sm': size === 'sm',
+    },
+  );
+
+  return (
+    <div
+      className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
+      onClick={handleBackgroundClick}
+    >
+      <div className={`${modalClass}`}>{children}</div>
+    </div>
+  );
+}
