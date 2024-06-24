be151c710ac7fdd329da6dc9c86de29f11d46889
yeonilil <gwagseoyeon@gmail.com>
2024-06-24 10:30:42 +0900

    style: 대쉬보드 헤더 반응형 스타일 추가

--- src/components/mydashboard/DashboardHeader.tsx
+++ src/components/mydashboard/DashboardHeader.tsx
@@ -1,14 +1,14 @@
 export default function DashBoardHeader() {
   return (
     <>
-      <nav className="z-998 right-0 top-0 flex h-70 w-full items-center justify-between border-1 border-b-gray-200 bg-white py-25 pe-80 ps-40">
+      <nav className="z-998 right-0 top-0 flex h-70 w-full items-center justify-between border-1 border-b-gray-200 bg-white py-25 ps-24 md:pe-40 md:ps-40 lg:pe-80 lg:ps-40">
         <p className="text-20 font-bold">내 대시보드</p>
         <ul className="flex items-center">
-          <li>
+          <li className="pl-12">
             <p className="flex h-38 w-38 items-center justify-center rounded-full border-2 border-white bg-orange-dashboard text-16 font-semibold text-white">{`K`}</p>
           </li>
-          <li className="ps-12">
-            <p className="text-16 font-medium">{`곽서연`}</p>
+          <li className="pl-12">
+            <p className="text-16 font-medium sm:hidden">{`곽서연`}</p>
           </li>
         </ul>
       </nav>
--- src/components/mydashboard/Sidemenu.tsx
+++ src/components/mydashboard/Sidemenu.tsx
@@ -7,12 +7,20 @@ export default function Sidemenu() {
       <nav className="z-999 left-0 top-0 h-screen w-300 border-1 border-r-gray-200 bg-white pl-24 pt-20">
         <Link
           href="/"
-          className="cursor-pointer text-25 font-bold text-toss-blue"
+          className="cursor-pointer text-25 font-bold text-toss-blue md:hidden lg:hidden"
+        >
+          :P
+        </Link>
+        <Link
+          href="/"
+          className="cursor-pointer text-25 font-bold text-toss-blue sm:hidden"
         >
           Planit:
         </Link>
         <div className="mt-50 flex justify-between pr-24">
-          <p className="text-12 font-bold text-gray-400">Dash Boards</p>
+          <p className="text-12 font-bold text-gray-400 sm:hidden">
+            Dash Boards
+          </p>
           <button>
             <Image
               src="/icon/add_box.svg"
