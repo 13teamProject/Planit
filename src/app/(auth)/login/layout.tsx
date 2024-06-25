e63d5a188b5607f512c37b0ad6bcae52d9c3ff6d
cindyCho <hjcho2303@naver.com>
2024-06-25 20:15:45 +0900

    💄 design: 디자인 추가 및 레이아웃 반응형 수정

--- src/app/(auth)/login/layout.tsx
+++ src/app/(auth)/login/layout.tsx
@@ -4,7 +4,7 @@ export default function Layout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="m-auto my-107 w-351 md:my-189 md:w-520 lg:my-173 lg:w-520">
+    <div className="m-auto my-107 w-full max-w-520 px-10 md:my-189 lg:my-173">
       {children}
     </div>
   );
