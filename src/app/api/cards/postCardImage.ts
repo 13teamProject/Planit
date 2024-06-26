e0026f3155c796f6135008301c58bd9687c76a5f
alexgoni <goni000211@gmail.com>
2024-06-26 09:56:19 +0900

    ✨ feat: fetch 함수

---
+++ src/app/api/cards/postCardImage.ts
@@ -0,0 +1,35 @@
+import { CardImageResponse, ErrorMessage } from '@planit-api';
+
+import { API_URL } from '../base-url';
+
+type Params = {
+  columnId: number;
+  image: File;
+};
+
+export default async function postCardImage({
+  columnId,
+  image,
+}: Params): Promise<CardImageResponse | ErrorMessage> {
+  const formData = new FormData();
+  formData.append('image', image);
+
+  const obj: RequestInit = {
+    method: 'POST',
+    body: formData,
+  };
+
+  try {
+    const res = await fetch(`${API_URL}/columns/${columnId}/card-image`, obj);
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message}`);
+
+    return data;
+  } catch (err) {
+    if (err instanceof Error) {
+      return { message: err.message };
+    }
+    return { message: '이미지 업로드 중 알 수 없는 오류가 발생했습니다.' };
+  }
+}
---
+++ src/app/api/cards/postCreateCard.ts
@@ -0,0 +1,30 @@
+import {
+  CreateCardRequest,
+  CreateCardResponse,
+  ErrorMessage,
+} from '@planit-api';
+
+import { API_URL, createFetchRequestOptions } from '../base-url';
+
+export default async function postCreateCard(
+  formValue: CreateCardRequest,
+): Promise<CreateCardResponse | ErrorMessage> {
+  const obj = createFetchRequestOptions('POST', {
+    ...formValue,
+  });
+
+  try {
+    const res = await fetch(`${API_URL}/cards`, obj);
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message}`);
+
+    return data;
+  } catch (err) {
+    if (err instanceof Error) {
+      return { message: err.message };
+    }
+
+    return { message: '카드 생성 중 알 수 없는 오류가 발생했습니다.' };
+  }
+}
