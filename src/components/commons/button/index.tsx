a0eb229d93b711d05532ab96f5b61cd0120af16b
JaesungKo <jaesungko.dev@gmail.com>
2024-06-26 09:15:43 +0900

    ♻️ refactor: 버튼 타입 상속 변경

--- src/components/commons/button/index.tsx
+++ src/components/commons/button/index.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import classNames from 'classnames';
+import { ButtonHTMLAttributes } from 'react';
 
-type ButtonProps = {
-  type?: 'button' | 'submit' | 'reset';
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   text: string;
   cancel?: boolean;
   style?: string;
@@ -11,20 +11,19 @@ type ButtonProps = {
 };
 
 export default function Button({
-  type = 'button',
   text,
   cancel = false,
   style,
   onClick,
 }: ButtonProps) {
-  const className = classNames('rounded-8 md:w-120 md:h-48 sm:w-138 sm:h-42', {
+  const className = classNames('rounded-8', {
     'bg-white border border-gray-200 text-gray-400 hover:bg-gray-200 hover:text-white':
       cancel,
     'bg-toss-blue text-white hover:bg-toss-blue/95': !cancel,
   });
 
   return (
-    <button type={type} className={`${className} ${style}`} onClick={onClick}>
+    <button className={`${className} ${style}`} onClick={onClick}>
       {text}
     </button>
   );
