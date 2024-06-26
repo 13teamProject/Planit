9056801f87c41a7c03590fdf025e39fdb448531e
kimhyesun <hyesun1004.kim@gmail.com>
2024-06-27 05:25:57 +0900

    ✨ feat: 비밀번호 변경 부분 UI 작업

---
+++ src/components/mypage/Password.tsx
@@ -0,0 +1,41 @@
+import { useForm } from 'react-hook-form';
+
+import Input from '../commons/input';
+
+export default function Password() {
+  const { register } = useForm();
+  return (
+    <div>
+      <h3 className="mb-32 text-24 font-bold">비밀번호 변경</h3>
+      <form>
+        <label htmlFor="password">현재 비밀번호</label>
+        <Input
+          id="password"
+          type="password"
+          placeholder="현재 비밀번호 입력"
+          size="md"
+          register={{ ...register('password', { required: true }) }}
+        />
+
+        <label htmlFor="password">새 비밀번호</label>
+        <Input
+          id="newPassword"
+          type="password"
+          placeholder="새 비밀번호 입력"
+          size="md"
+          register={{ ...register('newPassword', { required: true }) }}
+        />
+
+        <label htmlFor="password-confirm">새 비밀번호 확인</label>
+        <Input
+          id="password-confirm"
+          type="password"
+          placeholder="새 비밀번호 입력"
+          size="md"
+          register={{ ...register('password-confirm', { required: true }) }}
+        />
+        <button type="submit">변경</button>
+      </form>
+    </div>
+  );
+}
