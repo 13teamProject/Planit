7b7b369c831d787f2656ce65db326265a8a6d951
yeonilil <gwagseoyeon@gmail.com>
2024-06-27 15:19:04 +0900

    fix: husky 오류 수정

--- src/app/mydashboard/page.tsx
+++ src/app/mydashboard/page.tsx
@@ -1,55 +1,16 @@
 'use client';
 
-import { getDashboards, postDashboards } from '@/api/dashboards';
-import DashBoardHeader from '@/components/mydashboard/DashboardHeader';
-import Sidemenu from '@/components/mydashboard/Sidemenu';
-import { useEffect, useState } from 'react';
-
-type Dashboard = {
-  id: number;
-  title: string;
-  color: string;
-};
+import DashBoardHeader from '@/components/commons/layout/DashboardHeader';
+import Sidemenu from '@/components/commons/layout/Sidemenu';
 
 export default function MyDashBoardPage() {
-  const [dashboards, setDashboards] = useState<Dashboard[]>([]);
-
-  useEffect(() => {
-    async function fetchDashboard() {
-      const response = await getDashboards();
-      setDashboards(
-        response.dashboards.map((data) => ({
-          id: data.id,
-          title: data.title,
-          color: data.color,
-        })),
-      );
-    }
-
-    //fetchDashboard();
-    async function addDashboard() {
-      try {
-        const formData = { title: '비브리지', color: 'green-dashboard' }; // api로 데이터 받아올 예정
-        const response = await postDashboards(formData);
-        setDashboards([
-          ...dashboards,
-          ...response.dashboards.map((data) => ({
-            id: data.id,
-            title: data.title,
-            color: data.color,
-          })),
-        ]);
-      } catch (error) {
-        console.error('Failed to create dashboard:', error);
-      }
-    }
-    //addDashboard();
-  }, []);
-
   return (
-    <div className="flex">
-      <Sidemenu dashboards={dashboards} />
-      <DashBoardHeader isDashboard={true} />
+    <div className="flex h-screen">
+      <Sidemenu />
+      <div className="flex flex-1 flex-col">
+        <DashBoardHeader isDashboard />
+        <div className="flex-1">내용 작성</div>
+      </div>
     </div>
   );
 }
