a5a4544dd943694cb0a0775066e87194b924b8e0
kimhyesun <hyesun1004.kim@gmail.com>
2024-07-01 04:12:27 +0900

    ♻️ refactor: dashboard 하위 폴더명 변경 (Edit -> edit)

--- src/app/(dashboard)/edit/[id]/page.tsx
+++ src/app/(dashboard)/edit/[id]/page.tsx
@@ -1,6 +1,6 @@
-import DashboardInvitation from '@/components/dashboard/Edit/DashboardInvitation';
-import DashboardMember from '@/components/dashboard/Edit/DashboardMember';
-import DashboardName from '@/components/dashboard/Edit/DashboardName';
+import DashboardInvitation from '@/components/dashboard/edit/DashboardInvitation';
+import DashboardMember from '@/components/dashboard/edit/DashboardMember';
+import DashboardName from '@/components/dashboard/edit/DashboardName';
 
 export default function Edit({ params }: { params: { id: number } }) {
   return (
