d11df5be7a745ec075c9f8fb84e20c27dbbd7f6f
cindyCho <hjcho2303@naver.com>
2024-07-01 21:48:44 +0900

    feat: handleOutSide 훅 생성 및 드롭다운에 적용

--- src/components/dashboard/todo-detail-card/DropDownSelectBox.tsx
+++ src/components/dashboard/todo-detail-card/DropDownSelectBox.tsx
@@ -1,6 +1,7 @@
 import { deleteTodoCardDetails } from '@/app/api/cards';
 import Button from '@/components/commons/button';
 import Modal from '@/components/commons/modal';
+import useOutsideClick from '@/hooks/useOutsideClick';
 import { Dispatch, SetStateAction, useState } from 'react';
 
 type Props = {
@@ -15,7 +16,9 @@ export default function DropDownSelectBox({
   setSelectBoxIsOpen,
 }: Props) {
   const [deleteModalIsOpen, setDeleteModalIsOpen] = useState(false);
-
+  const dropdownRef = useOutsideClick<HTMLUListElement>(() => {
+    setSelectBoxIsOpen(false);
+  });
   const openDeleteModal = () => {
     setDeleteModalIsOpen(true);
   };
@@ -30,7 +33,10 @@ export default function DropDownSelectBox({
   };
 
   return (
-    <ul className="mt-1 flex h-74 w-86 flex-col gap-1 rounded-b-xl rounded-t-xl border bg-white p-6 text-center sm:right-0 md:h-82 md:w-93 md:gap-6">
+    <ul
+      ref={dropdownRef}
+      className="mt-1 flex h-74 w-86 flex-col gap-1 rounded-b-xl rounded-t-xl border bg-white p-6 text-center sm:right-0 md:h-82 md:w-93 md:gap-6"
+    >
       <li>
         <button
           className="h-30 w-74 rounded-4 pt-3 text-12 hover:bg-toss-blue-light/40 hover:text-toss-blue md:h-32 md:w-81 md:text-14"
