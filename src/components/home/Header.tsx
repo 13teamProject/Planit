9249028bce1c1a9e429215d09cb7f4ce663c4d32
kimhyesun <hyesun1004.kim@gmail.com>
2024-07-08 15:55:27 +0900

    ♻️ refactor: 사용하지 않는 code 삭제

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import classNames from 'classnames';
-import { motion, spring } from 'framer-motion';
+import { motion } from 'framer-motion';
 import Image from 'next/image';
 import Link from 'next/link';
 
